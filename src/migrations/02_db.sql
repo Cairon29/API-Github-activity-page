f1db4c1bfcd61b7f5cf586d535f58f65c491bec7
Cairon29 <josewjjunco@gmail.com>
2026-01-01 20:11:23 -0500

    feat: DB V2 migration added

---
+++ src/migrations/02_db.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS ur_commit;
+CREATE DATABASE ur_commit;
+use ur_commit;
+
+CREATE TABLE users(
+	id 
+		binary(16) 
+        PRIMARY KEY 
+        NOT NULL 
+        DEFAULT (UUID_TO_BIN(UUID())),
+    supabase_uid char(36) not null unique,
+    email varchar(100) not null,
+    fullname varchar(150),
+    github_username varchar(150),
+    phone varchar(10),
+    
+	current_strike int default 0,
+    longest_strike int default 0,
+    public_profile boolean default true,
+    member_since 
+		timestamp 
+        NOT NULL 
+        DEFAULT current_timestamp,
+    is_admin boolean default false
+);
+
+CREATE TABLE friends_request (
+	id int auto_increment primary key not null,
+    status 
+		ENUM('pending', 'rejected', 'accepted') 
+        not null 
+        default 'pending',
+    sending_date 
+		timestamp 
+        NOT NULL 
+        default current_timestamp,
+    addressee_id binary(16),
+    sender_id binary(16),
+    foreign key (addressee_id) REFERENCES users(id),
+    foreign key (sender_id) REFERENCES users(id)
+);
+
+CREATE TABLE notification_config(
+	id int auto_increment primary key not null,
+    fk_id_user binary(16),
+    active boolean default true,
+    set_hour timestamp not null,
+    whatsapp_notify boolean,
+    email_notify boolean,
+    mobile_app_notigy boolean,
+    tittle_message varchar(20),
+    description_message varchar(70),
+    strike boolean,
+    foreign key (fk_id_user) REFERENCES users(id)
+);
+
+
+CREATE TABLE dashboard_config(
+	id int auto_increment primary key not null,
+    fk_id_user binary(16),
+    properties json
+);
